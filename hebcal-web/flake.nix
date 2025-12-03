e8e7b776151ec187c303bc13cb80de73800cee9e
Doron Behar <doron.behar@gmail.com>
2025-12-03 21:56:02 +0200

    Add flake for hebcal-web

---
+++ hebcal-web/flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Hebcal web interface development flake";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs, }: {
+    devShell.x86_64-linux = with (import nixpkgs {system = "x86_64-linux";}); mkShell {
+      nativeBuildInputs = [
+        nodePackages_latest.node-gyp
+        nodePackages_latest.nodejs
+        sqlite
+      ];
+    };
+  };
+}
